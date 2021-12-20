75099eefda4bfaa91bafc7650996afe07a4ec6c3
DenisZ <zayczev2012@mail.ru>
2021-12-20 20:56:14 +0500

    task4

---
+++ Базовые/task4.js
@@ -0,0 +1,25 @@
+elem4.onclick = function() {
+  let val1 = document.getElementById('input1_task4').value;
+  weakday(val1);
+};
+
+function weakday(day) {
+  switch (day) {
+    case "1":
+      return document.getElementById("result4").innerHTML = `Понедельник`;
+    case "2":
+      return document.getElementById("result4").innerHTML = `Вторник`;
+    case "3":
+      return document.getElementById("result4").innerHTML = `Среда`;
+    case "4":
+      return document.getElementById("result4").innerHTML = `Четверг`;
+    case "5":
+      return document.getElementById("result4").innerHTML = `Пятница`;
+    case "6":
+      return document.getElementById("result4").innerHTML = `Суббота`;
+    case "7":
+      return document.getElementById("result4").innerHTML = `Воскрсенье`;
+    default:
+      alert('Yander dev.');
+  }
+}
