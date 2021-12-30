e2deedb5cb215ce2de29cc40a518b36306be4749
DenisZ <zayczev2012@mail.ru>
2021-12-30 15:49:19 +0500

    main

--- Объекты/task3.js
+++ Объекты/task3.js
@@ -1,20 +1,23 @@
 (function() {
-  let arr = [];
-  elem3.addEventListener("click", task3);
 
-  function task3() {
-    let a = document.getElementById('input1_task3').valueAsNumber;
-    arr.push(a);
-    document.getElementById("result4").innerHTML = `Массив: ${arr}`;
-    massive(arr);
-  };
-
-  function massive(arr) {
-    const getAverage = (arr) => {
-      const sum = arr.reduce((acc, number) => acc + number, 0);
-      const length = arr.length;
-      return sum / length;
-    };
-    return document.getElementById("result5").innerHTML = `Среднее арифметическое : ${(Math.round(getAverage(arr)))}`;
+  let ladder = {
+    steps: 0,
   }
+
+  function up() {
+    ladder.steps += 1;
+    document.getElementById("result4").innerHTML = `Ступенька: ${ladder.steps}`;
+    
+  }
+
+
+
+  function down() {
+    ladder.steps -= 1;
+    document.getElementById("result4").innerHTML = `Ступенька: ${ladder.steps}`;
+  }
+
+
+  elem3.addEventListener("click", up);
+  elem4.addEventListener("click", down);
 })();
